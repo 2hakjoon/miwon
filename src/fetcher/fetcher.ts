b8ac4f665aa73ce954235e9551454041ec5f9ba2
HAKJOON0223 <2hakjoon@gmail.com>
2022-09-16 19:49:02 +0900

    ADD - fetcher with mockdata

---
+++ src/fetcher/fetcher.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+interface FetcherRequestConfig {
+  headers?: Record<string, string | number | boolean>
+  baseUrl?: string
+}
+
+export const createFetcher = (config: FetcherRequestConfig) => {
+  const fetch = axios.create(config)
+
+  return {
+    get: <T>(url: string): Promise<T> => fetch.get(url).then(res => res.data),
+
+    post: <T>(url: string, body: object): Promise<T> =>
+      fetch.post(url, body).then(res => res.data),
+
+    delete: <T>(url: string): Promise<T> =>
+      fetch.delete(url).then(res => res.data),
+
+    put: <T>(url: string, body: object): Promise<T> =>
+      fetch.put(url, body).then(res => res.data)
+  }
+}
