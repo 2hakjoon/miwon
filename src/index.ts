6af87f3d8fb4cb1f1bbdcf0f9b68421cb52bf657
HAKJOON0223 <2hakjoon@gmail.com>
2022-12-29 22:49:31 +0900

    ADD - addMiwon return type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from 'normalizr'
 export * from './addMiwon'
 
-export { AddMiwon } from './types/addMiwon'
+export { AddMiwonArgs, AddMiwonReturns } from './types/addMiwon'
 export { FetcherRequestConfig } from './types/createFetcher'
 export { Store } from './types/createStore'
--- src/types/addMiwon.ts
+++ src/types/addMiwon.ts
@@ -1,6 +1,13 @@
 import { FetcherRequestConfig } from './createFetcher'
+import { Store } from './createStore'
+import { Subscription } from './subscription'
 
-export interface AddMiwon {
+export interface AddMiwonArgs {
   config?: FetcherRequestConfig
   initVal?: { [k: string]: any }
+}
+
+export interface AddMiwonReturns extends Subscription, Store {
+  miwonQuery: (url: string, func: (res: any) => any) => any
+  miwonMutation: (url: string, body: any) => any
 }
