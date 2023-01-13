0bf8cca938260d43cb498c88d9e3cca2dfcd1eae
HAKJOON0223 <2hakjoon@gmail.com>
2023-01-14 00:50:32 +0900

    CONFIG - build config and version up

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,37 @@
 import typescript from 'rollup-plugin-typescript2'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+import babel from '@rollup/plugin-babel'
+import json from '@rollup/plugin-json'
+
+import pkg from './package.json'
 
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      file: './dist/index.js',
-      format: 'cjs'
-    },
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
     plugins: [
+      commonjs(),
+      // resolve(),
+      json(),
+      peerDepsExternal(),
+      babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
       typescript({
         tsconfig: 'tsconfig.json'
       })
     ]
-  },
+  }
 ]
